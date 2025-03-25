4a7cb991bd961b8b97b6bfd7836d7aae4c7fee8b
unknown <obisesanolabode10@gmail.com>
2025-03-25 06:21:45 +0100

    updated version of invoice management system

---
+++ invoice_mgmt/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .forms import InvoiceForm
+from .models import Invoice
+
+# Create your views here.
+def home_page(request):
+    """This is a view function that takes in request for the home page and renders the contents"""
+    title = "Welcome Gee: You are now on the home page"
+    context = {
+        "title" : title
+    }
+    return render(request, "home.html", context)
+
+def add_invoice(request):
+    """This is a view function that adds a new invoice and displays the contents"""
+    form = InvoiceForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect ('/add_invoice')
+    context = {
+        "form" : form,
+        "Title" : "NEW INVOICE"
+    }
+    return render(request, "new_invoice.html", context)
+
+def list_invoice_items(request):
+    """function that lists the invoice items by querying the database"""
+    title = "List of Invoices"
+    queryset = Invoice.objects.all()
+    context = {
+        "title" : title,
+        "queryset" : queryset
+    }
+    return render(request, "list_invoice_items.html", context)
