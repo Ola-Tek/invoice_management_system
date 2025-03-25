2023abf04676b27f3a24272c663f59776db21ebd
unknown <obisesanolabode10@gmail.com>
2025-03-25 06:39:47 +0100

    updated version

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
